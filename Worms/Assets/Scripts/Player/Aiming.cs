b93666c5fd3d760cdf8ebb9366437f9a4fcd0bdc
Joe Binns <joebinns.95@gmail.com>
2022-09-17 22:56:43 +0200

    Fix mouse using delta instead of position

--- Worms/Assets/Scripts/Player/Aiming.cs
+++ Worms/Assets/Scripts/Player/Aiming.cs
@@ -15,13 +15,15 @@ public class Aiming : MonoBehaviour
     // 3.   Shrink reticle
 
     private Vector2 _lookPosition;
-    private Vector2 _mouseOffset;
+    private Vector2 _mouseOffset = Vector2.zero;
+    private Vector2 _mousePosition = Vector2.zero;
 
     private PhysicsBasedCharacterController _characterController;
 
     private void Start()
     {
         _characterController = PlayerManager.currentPlayer.GetComponent<PhysicsBasedCharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
@@ -35,7 +37,10 @@ public class Aiming : MonoBehaviour
         // Set _characterLookDirection to aiming
         _characterController._characterLookDirection = PhysicsBasedCharacterController.lookDirectionOptions.aiming;
         // Reset mouse position to centre ?
-        _mouseOffset = _lookPosition; // Only use mouseOffset if Keyboard and Mouse is being used
+        //_mouseOffset = _lookPosition; // Only use mouseOffset if Keyboard and Mouse is being used
+        _mousePosition = Vector2.zero;
+
+        // Make the _characterController look spring much faster and less dampened... hmmm... bit janky
     }
 
     private void OnDisable()
@@ -46,17 +51,21 @@ public class Aiming : MonoBehaviour
 
     public void LookPosition(InputAction.CallbackContext context)
     {
-        _lookPosition = context.ReadValue<Vector2>();
+        _lookPosition = context.ReadValue<Vector2>(); // Actually, this is DELTA now
     }
 
     private void Update()
     {
+        Debug.Log(_mousePosition);
+
+        _mousePosition += _lookPosition * Time.deltaTime;
+        // HMMM... using mouse delta works well. BUT, doesn't work when the mouse ISN'T moving...
+
         // Somehow now need to allow aimingInput to loop... get _lookPosition in a different way?
-        var xSensitivity = 0.01f;
-        var ySensitivity = 0.1f;
-        Debug.Log(_mouseOffset);
+        var xSensitivity = 0.01f * 1000f;
+        var ySensitivity = 0.1f * 1000f;
         // _mouseOffset.y doesn't seem to be working for some reason... (?)
-        var aimingInput = new Vector3(Mathf.Sin((_lookPosition.x - _mouseOffset.x) / 360f * 2 * Mathf.PI * xSensitivity), (_lookPosition.y -_mouseOffset.y) / 360f * ySensitivity, Mathf.Cos((_lookPosition.x - _mouseOffset.x)  / 360f * 2 * Mathf.PI  * xSensitivity));
+        var aimingInput = new Vector3(Mathf.Sin((_mousePosition.x) / 360f * 2 * Mathf.PI * xSensitivity), (_mousePosition.y) / 360f * ySensitivity, Mathf.Cos((_mousePosition.x)  / 360f * 2 * Mathf.PI  * xSensitivity));
         //Debug.Log(aimingInput);
         _characterController._aimingInput = aimingInput;
 
