18f659ce1755bcf38c002d6fc905a1d6c5f5903e
Joe Binns <joebinns.95@gmail.com>
2022-09-25 13:28:44 +0200

    Add various hats, and name input

--- Worms/Assets/Scripts/Player/Hat.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/HatScriptableObject", order = 1)]
-public class HatScriptableObject : ScriptableObject
-{
-    public string hatName;
-
-    public GameObject hatPrefab;
-}
---
+++ Worms/Assets/Scripts/Player/HatSetting.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Hat Setting")]
+public class HatSettings : ScriptableObject
+{
+    public string hatTextEffect;
+
+    public GameObject hatPrefab;
+}
